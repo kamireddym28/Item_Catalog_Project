79083e157c41a273adc14f55db4a2f4762ece3d2
Mounica Kamireddy <kamireddym28@gmail.com>
2018-03-17 01:03:44 -0700

    Update Catalog_project.py

--- Catalog_project.py
+++ Catalog_project.py
@@ -86,6 +86,7 @@ def gconnect():
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's"
         response.headers['Content-Type'] = 'application/json'
         return response
 
